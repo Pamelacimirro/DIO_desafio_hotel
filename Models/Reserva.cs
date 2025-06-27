021174ddc63c7619122bae381b3f9889bed5f66b
Pedro Ribeiro Martins Rosa <pamela.cimirro@gmail.com>
2025-06-27 16:44:32 -0300

    atualizaçoes...ainda faltando lista de suites

--- Models/Reserva.cs
+++ Models/Reserva.cs
@@ -6,8 +6,11 @@ namespace DesafioProjetoHospedagem.Models
         public Suite Suite { get; set; }
         public int DiasReservados { get; set; }
 
-        public Reserva() { }
-
+        public Reserva()
+        {
+            Hospedes = new List<Pessoa>();
+        }
+        
         public Reserva(int diasReservados)
         {
             DiasReservados = diasReservados;
@@ -15,6 +18,44 @@ namespace DesafioProjetoHospedagem.Models
 
         public void CadastrarHospedes(List<Pessoa> hospedes)
         {
+            Console.WriteLine("Digite o primeiro nome do hóspede:");
+            Pessoa p1 = new Pessoa();
+            string nome = Console.ReadLine();
+            p1.Nome = nome;
+            Console.WriteLine("Digite o sobrenome do hóspede:");
+            string sobrenome = Console.ReadLine();
+            p1.Sobrenome = sobrenome;
+            hospedes.Add(p1);
+
+            Console.WriteLine("Deseja adicionar mais hóspedes? (s/n)");
+            string resposta = Console.ReadLine().ToLower();
+            do
+            {
+
+                if (resposta == "s")
+                {
+                    Console.WriteLine("Digite o primeiro nome do próximo hóspede:");
+                    Pessoa p2 = new Pessoa();
+                    string nome2 = Console.ReadLine();
+                    p2.Nome = nome2;
+                    Console.WriteLine("Digite o sobrenome do próximo hóspede:");
+                    string sobrenome2 = Console.ReadLine();
+                    p2.Sobrenome = sobrenome2;
+                    hospedes.Add(p2);
+                }
+                else if (resposta != "n")
+                {
+                    throw new Exception("Opção inválida. Digite 's' para sim ou 'n' para não.");
+                }
+                else
+                {
+                    Console.WriteLine($"Foram adicionados {hospedes.Count} hóspedes.");
+                }
+
+            }
+            while (resposta == "s");
+            
+
             // TODO: Verificar se a capacidade é maior ou igual ao número de hóspedes sendo recebido
             if (Suite != null && hospedes.Count <= Suite.Capacidade)
             {
@@ -29,7 +70,30 @@ namespace DesafioProjetoHospedagem.Models
 
         public void CadastrarSuite(Suite suite)
         {
+            Console.WriteLine("Escolha a suíte para a reserva:");
+            Console.WriteLine("1 - Simples");
+            Console.WriteLine("2 - Luxo");
+            Console.WriteLine("3 - Premium");
+            int escolha_suite = int.Parse(Console.ReadLine());
+
+            if (escolha_suite == 1)
+            {
+                suite = new Suite("Simples", 2, 100);
+            }
+            else if (escolha_suite == 2)
+            {
+                suite = new Suite("Luxo", 4, 200);
+            }
+            else if (escolha_suite == 3)
+            {
+                suite = new Suite("Premium", 5, 300);
+            }
+            else
+            {
+                throw new Exception("Opção de suíte inválida.");
+            }
             Suite = suite;
+            Console.Clear();
         }
 
         public int ObterQuantidadeHospedes()
--- Models/Suite.cs
+++ Models/Suite.cs
@@ -9,10 +9,15 @@ namespace DesafioProjetoHospedagem.Models
             TipoSuite = tipoSuite;
             Capacidade = capacidade;
             ValorDiaria = valorDiaria;
+            
         }
 
         public string TipoSuite { get; set; }
         public int Capacidade { get; set; }
         public decimal ValorDiaria { get; set; }
+        
+
+        
+
     }
 }
